110fc00c8ab64c31b2ae6b79967f4f58dd5a3159
sikamloïque <loicsikam272@gmail.com>
2021-12-10 16:29:27 +0100

    modifications du 10/12

--- src/app/ambassadeur/mon-compte/porte-monnaie/porte-monnaie.page.ts
+++ src/app/ambassadeur/mon-compte/porte-monnaie/porte-monnaie.page.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorteMonnaiePage implements OnInit {
 
-  constructor() { }
+  items =[
+    {periode:'Cette semaine', prix:'65€'},
+    {periode:'Ce moic-ci', prix:'450€'},
+    {periode:'Cette année', prix:'1500€'},
+    {periode:'En attente de virement', prix:'35€'},
+  ]
+  constructor(private location : Location) { }
+
+  myBackButton(){
+    this.location.back();
+  }
 
   ngOnInit() {
   }
